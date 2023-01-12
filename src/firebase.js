9168648fcea77e827f9d650ad4c7a49c871bcdbd
Athithyen <athithyen11@gmail.com>
2023-01-12 13:27:02 -0500

    netflix

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBlg0Lc_4QGq5QmklQk66kPvI7uTDZPx0M",
+  authDomain: "netflix-clone-a99e7.firebaseapp.com",
+  projectId: "netflix-clone-a99e7",
+  storageBucket: "netflix-clone-a99e7.appspot.com",
+  messagingSenderId: "822820016339",
+  appId: "1:822820016339:web:760d7780328adab519c426"
+};
